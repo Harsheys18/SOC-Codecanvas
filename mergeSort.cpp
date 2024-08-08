47979769438c375ecfd3af0d88219b8b85907b8f
Harsheys18 <157981579+Harsheys18@users.noreply.github.com>
2024-08-08 20:35:10 +0530

    First commit

---
+++ mergeSort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+struct merged_array{
+    int array[100];
+};
+
+merged_array merge(int L[],int R[]){
+    int m=sizeof(L),i=0;
+    int n=sizeof(R),j=0;
+    int array[100];
+    for(int k=0;k<m+n;k++){
+        
+    }
+}
+
+int main(){
+
+}
